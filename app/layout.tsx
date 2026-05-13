231ea7a188fe27474643b422575182581fa18781
MichaelSugden <michael@digitlsky.com>
2026-05-13 14:40:05 +0200

    Update layout.tsx
    Added GTM to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Rozha_One, Poppins, Lora } from 'next/font/google';
 import "./globals.css";
 import siteSettings from "@/data/settings.json";
+import Script from 'next/script'
 
 const rozhaOne = Rozha_One({
   weight: '400',
@@ -116,10 +117,27 @@ export const viewport = {
   themeColor: "#0F1C1C",
 };
 
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* GTM Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WLS465WG');
+            `,
+          }}
+        />
+        
         {/* Removed manual <link rel="icon"> — Next.js will emit it from metadata.icons */}
         {/* Structured Data: Organization */}
         <script
@@ -184,7 +202,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }}
         />
       </head>
-      <body className={`${poppins.variable} ${lora.variable} ${rozhaOne.variable} font-poppins antialiased`}>{children}</body>
+      <body className={`${poppins.variable} ${lora.variable} ${rozhaOne.variable} font-poppins antialiased`}>{children}
+        {/* GTM The noscript part (Optional but recommended) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLS465WG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+      </body>
     </html>
   );
 }
